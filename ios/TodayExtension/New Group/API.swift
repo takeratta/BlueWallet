b70a2b84ef35cf54b2bd92ef69e330b5627e6b2a
Marcos Rodriguez <marcospr@pm.me>
2019-11-02 20:25:55 -0400

    ADD: Initial Today Extension. USD only at the moment.
    Update MainInterface.storyboard
    
    ADD: Cache fetched data
    
    ADD: Add "Up from/Down from" data
    
    OPS: Add Provision Profile
    
    Update project.pbxproj
    
    ADD: Synchronize user selected currency with Today extension
    
    Update Info.plist

---
+++ ios/TodayExtension/New Group/API.swift
@@ -0,0 +1,46 @@
+//
+//  API.swift
+//  TodayExtension
+//
+//  Created by Marcos Rodriguez on 11/2/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+
+class API {
+      
+  static func fetchPrice(currency: String, completion: @escaping ((Dictionary<String, Any>?, Error?) -> Void)) {
+    guard let url = URL(string: "https://api.coindesk.com/v1/bpi/currentPrice/\(currency).json") else {return}
+    
+    URLSession.shared.dataTask(with: url) { (data, response, error) in
+       guard let dataResponse = data,
+        let json = try? JSONSerialization.jsonObject(with: dataResponse, options: .mutableContainers) as? Dictionary<String, Any>,
+        error == nil else {
+          print(error?.localizedDescription ?? "Response Error")
+          completion(nil, error)
+          return }
+      
+      completion(json, nil)
+    }.resume()
+  }
+  
+  static func getUserPreferredCurrency() -> String {
+    guard let userDefaults = UserDefaults(suiteName: "group.io.bluewallet.bluewallet"),
+      let preferredCurrency = userDefaults.value(forKey: "preferredCurrency") as? String
+      else {
+        return "USD"
+    }
+    return preferredCurrency
+  }
+  
+  static func getUserPreferredCurrencyLocale() -> String {
+    guard let userDefaults = UserDefaults(suiteName: "group.io.bluewallet.bluewallet"),
+      let preferredCurrency = userDefaults.value(forKey: "preferredCurrencyLocale") as? String
+      else {
+        return "en_US"
+    }
+    return preferredCurrency
+  }
+  
+}
