6aab422e8750fbaca768dfed996695f2f7e23e15
Marcos Rodriguez <marcospr@pm.me>
2019-11-06 01:00:37 -0500

    ADD: quick actions
    Update App.js
    
    Update App.js
    
    Update App.js
    
    Update App.js
    
    Update App.js
    
    FIX: Fix currency bugs
    
    FIX: Hide balance from quick actions
    
    ADD: cqc
    
    Update currency.js
    
    Update TodayViewController.swift
    
    Update App.js
    
    REF: headercolor
    
    Update App.js
    
    FIX: carousel would not show
    
    FIX: Fix
    
    FIX
    
    FIX
    
    Update API.swift
    
    FIX: Fixed alerts
    
    TST
    
    Revert "TST"
    
    This reverts commit bcdf62e9b238cc5da200cc4bce45e4e55f480d6d.

--- ios/TodayExtension/New Group/API.swift
+++ ios/TodayExtension/New Group/API.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 class API {
-      
+  
   static func fetchPrice(currency: String, completion: @escaping ((Dictionary<String, Any>?, Error?) -> Void)) {
     guard let url = URL(string: "https://api.coindesk.com/v1/bpi/currentPrice/\(currency).json") else {return}
     
     URLSession.shared.dataTask(with: url) { (data, response, error) in
-       guard let dataResponse = data,
+      guard let dataResponse = data,
         let json = try? JSONSerialization.jsonObject(with: dataResponse, options: .mutableContainers) as? Dictionary<String, Any>,
         error == nil else {
           print(error?.localizedDescription ?? "Response Error")
@@ -27,20 +27,39 @@ class API {
   
   static func getUserPreferredCurrency() -> String {
     guard let userDefaults = UserDefaults(suiteName: "group.io.bluewallet.bluewallet"),
-      let preferredCurrency = userDefaults.value(forKey: "preferredCurrency") as? String
+      let preferredCurrency = userDefaults.string(forKey: "preferredCurrency")
       else {
         return "USD"
     }
+    
+    if preferredCurrency != API.getLastSelectedCurrency() {
+      UserDefaults.standard.removeObject(forKey: TodayData.TodayCachedDataStoreKey)
+      UserDefaults.standard.removeObject(forKey: TodayData.TodayDataStoreKey)
+      UserDefaults.standard.synchronize()
+    }
+    
     return preferredCurrency
   }
   
   static func getUserPreferredCurrencyLocale() -> String {
     guard let userDefaults = UserDefaults(suiteName: "group.io.bluewallet.bluewallet"),
-      let preferredCurrency = userDefaults.value(forKey: "preferredCurrencyLocale") as? String
+      let preferredCurrency = userDefaults.string(forKey: "preferredCurrencyLocale")
       else {
         return "en_US"
     }
     return preferredCurrency
+  }
+  
+  static func getLastSelectedCurrency() -> String {
+    guard let dataStore = UserDefaults.standard.string(forKey: "currency") else {
+      return "USD"
+    }
+    
+    return dataStore
+  }
+  
+  static func saveNewSelectedCurrency() {
+    UserDefaults.standard.setValue(API.getUserPreferredCurrency(), forKey: "currency")
   }
   
 }
